23366c595d22fa48c4a3b0e7a9e5611618caccf8
Ntsanii <ntsane01m@gmail.com>
2023-10-18 12:55:04 -0700

    With a face like mine, I do better in print

--- main.h
+++ main.h
@@ -7,6 +7,9 @@ int printf_char(va_list val);
 int printf_string(va_list val);
 int _strlen(char *str);
 int _strlenc(const char *str);
-int print_37(void)
+int print_37(void);
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_bin(va_list val);
 
 #endif
